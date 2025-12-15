5889ad7af08884d1ebf9859fc3c9e369e38be214
abumusaalashari <aridrahman42@gmail.com>
2025-12-15 14:25:37 +0600

    17-33225-1

---
+++ project/lab11/interfaceIndexer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+interface IStudent
+{
+    void Displayinfo();
+}
+class Student : IStudent
+{
+    private string name;
+    private int[] marks = new int[3];
+    public Student(string name)
+    {
+        this.name = name;
+    }
+    public int this[int index]
+    {
+        get { return marks[index]; }
+        set { marks[index] = value; }
+    }
+    public void Displayinfo()
+    {
+        Console.WriteLine("Student Name: " + name);
+        for (int i = 0; i < marks.Length; i++)
+        {
+            Console.WriteLine("Mark " + (i + 1) + ": " + marks[i]);
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student("Ashari");
+        s[0] = 70;
+        s[1] = 75;
+        s[2] = 80;
+        s.Displayinfo();
+    }
+}
---
+++ project/lab11/interfacePRACTICE/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+interface IPrintable
+{
+    void Print();
+}
+interface ISerializable
+{
+    void save();
+}
+class Documents: IPrintable, ISerializable
+{
+    public void Print()
+    {
+        Console.WriteLine("Printing the document");
+    }
+
+    public void save()
+    {
+        Console.WriteLine("Saving the document");
+            }
+}
+
+class Program 
+{
+    static void Main()
+    {
+        Documents doc = new Documents();
+        doc.Print();
+        doc.save();
+    }
+}
